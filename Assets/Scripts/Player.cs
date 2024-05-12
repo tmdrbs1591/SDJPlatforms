c4e873d888c0368407fcdec7ec3375e2266678fd
tmdrbs1591 <sdh230211@sdh.hs.kr>
2024-05-12 15:51:07 +0900

    Player Animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("플레이어 스텟")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpPower;
-    [SerializeField] private Transform overlapCirclePos;
+    [SerializeField] private float attackcoolTime = 0.5f;
+    [SerializeField] private float slidingSpeed;
+   
+    [Space]
+    [SerializeField] private Transform overlapCirclePos;//플레이어 바닥 감지 위치
     [SerializeField] private float checkRadius;
     [SerializeField] private LayerMask isLayer;
 
-    [SerializeField] private float AttackcoolTime = 0.5f;
-    [SerializeField] private float AttackcurTime;
+    [SerializeField] private Transform wallChk;
+    [SerializeField] private float wallchkDistance;
+    [SerializeField] private LayerMask w_Layer;
 
+
+
+
+    private float AttackcurTime;
     private float attackBuffer;
     private int attackIndex;
     private bool isAttack;
+    private bool isWall;
+    [SerializeField] private bool isFall;
+    private float isRight;
 
     private Rigidbody2D rigid;
     [SerializeField] private Animator anim;
@@ -32,7 +45,18 @@ public class Player : MonoBehaviour
         Move();
         Jump();
         Attack();
+        Wall();
 
+    }
+    void Wall() // 벽타기
+    {
+        isWall = Physics2D.Raycast(wallChk.position, Vector2.right * isRight, wallchkDistance,w_Layer);
+        anim.SetBool("isSliding", isWall);
+
+        if (isWall)
+        {
+            rigid.velocity = new Vector2(rigid.velocity.x, rigid.velocity.y * slidingSpeed);
+        }
     }
     void Move() // 움직이는 함수
     {
@@ -44,38 +68,40 @@ public class Player : MonoBehaviour
         if (!isAttack) rigid.velocity = new Vector2(x * moveSpeed, rigid.velocity.y);
 
 
-        if (Mathf.Abs(rigid.velocity.x) > 0.1f)
+        if (Mathf.Abs(rigid.velocity.x) > 0.1f) // 좌우반전
         {
-            if (rigid.velocity.x < 0) transform.localScale = new Vector3(-1f, 1f, 1f);
-            if (rigid.velocity.x > 0) transform.localScale = new Vector3(1f, 1f, 1f);
+            transform.localScale = new Vector3(isRight, 1f, 1f);
+            if (rigid.velocity.x < 0) isRight = -1f;
+            if (rigid.velocity.x > 0) isRight = 1f;
         }
         if (Mathf.Abs(rigid.velocity.x) > 0) anim.SetBool("isRun", true);
         else anim.SetBool("isRun", false);
        
     }
-    void Jump()
+    void Jump() // 점프
     {
         isGround = Physics2D.OverlapCircle(overlapCirclePos.position, checkRadius, isLayer);
+        isFall = rigid.velocity.y < 0;
         if (isGround)
         {
-            anim.SetBool("isFall", false);
+            anim.SetBool("isFall", isFall);
             if (Input.GetKeyDown(KeyCode.Z))
             {
                 rigid.velocity = Vector2.up * jumpPower;
                 anim.SetTrigger("isJump");
             }
         }
-        if (!isAttack)
-            if (rigid.velocity.y < 0) anim.SetBool("isFall", true);
+            if (rigid.velocity.y < 0) anim.SetBool("isFall", isFall);
+
 
     }
-    void Attack()
+    void Attack() // 공격
     {
         attackBuffer -= Time.deltaTime;
         if (attackBuffer <= 0) attackIndex = 1;
         if (Input.GetKeyDown(KeyCode.X))
             attackBuffer = 0.329f;
-
+        
         if (AttackcurTime <= 0)
         {
             if (attackBuffer > 0)
@@ -83,12 +109,12 @@ public class Player : MonoBehaviour
                 switch (attackIndex)
                 {
                     case 1:
-                        AttackcurTime = AttackcoolTime;
-                        StartCoroutine(IsAttacking(AttackcoolTime));
+                        AttackcurTime = attackcoolTime;
+                        StartCoroutine(IsAttacking(attackcoolTime));
                         break;
                     case 2:
-                        AttackcurTime = AttackcoolTime;
-                        StartCoroutine(IsAttacking(AttackcoolTime));
+                        AttackcurTime = attackcoolTime;
+                        StartCoroutine(IsAttacking(attackcoolTime));
                         break;
                
                 }
@@ -108,5 +134,11 @@ public class Player : MonoBehaviour
         yield return new WaitForSeconds(attackTime);
         isAttack = false;
 
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(wallChk.position, Vector2.right * isRight * wallchkDistance);
     }
 }
